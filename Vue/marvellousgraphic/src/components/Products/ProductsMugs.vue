8f8f977bef409480c5d905a91b612ecacb74a703
ShanmuCode27 <90304223+ShanmuCode27@users.noreply.github.com>
2022-06-29 21:24:53 +0530

    nothing

--- Vue/marvellousgraphic/src/components/Products/ProductsMugs.vue
+++ Vue/marvellousgraphic/src/components/Products/ProductsMugs.vue
@@ -13,8 +13,25 @@
               <button> {{card.buttonTitle}}</button>
           </div>
       </div>
-      
   </div>
+
+  <div class="dropdown">
+        <div class="dropdown__header" @click="toggleDropdown($event)">
+            <span>Header</span> text
+            <i class="fa fa-angle-down" aria-hidden="true"></i>
+            <i class="fa fa-angle-up" aria-hidden="true"></i>
+        </div>
+        
+        <div class="dropdown__content">
+            <ul>
+                <li>Lorem ipsum</li>
+                <li>Lorem ipsum dolor sit</li>
+                <li>Lorem ipsum dolor</li>
+            </ul>
+        </div>
+    </div>
+
+
 </template>
 
 <script>
@@ -34,7 +51,12 @@ export default {
             ],
 
         }
-    }
+    },
+     methods: {
+            toggleDropdown (event) {
+                event.currentTarget.classList.toggle('is-active');
+            }
+        }
 }
 </script>
 
@@ -79,4 +101,37 @@ export default {
     font-size: 15px;
 }   
 
+
+  &__header {
+            cursor: pointer;
+            line-height: 50px;
+            padding-left: 10px;
+            padding-right: 50px;
+            position: relative;
+            text-overflow: ellipsis;
+  }
+    i.fa {
+                position: absolute;
+                right: 10px;
+                top: 50%;
+                transform: translateY(-50%);
+                transition: opacity .3s;
+               
+            }
+
+             &.fa-angle-up {
+                    opacity: 0;
+                }
+
+             &.fa-angle-down {
+                        opacity: 0;
+                    }
+            &.is-active i.fa, &.fa-angle-up {
+                        opacity: 1;
+                    }
+                + .dropdown__content {
+                    height: auto;
+                    opacity: 1;
+                    visibility: visible;
+                }
 </style>
